8d061c2f6db53936fc12d5118b1a016d77b86f37
Joshua Newton <joshua.newton102@gmail.com>
2023-01-08 11:16:42 -0600

    Base project setup

---
+++ src/mapper.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from bokeh.driving import count
+from bokeh.layouts import column, gridplot, row
+from bokeh.models import ColumnDataSource, Select, Slider
+from bokeh.plotting import figure
+
+
+if __name__ == "__main__":
+    x = figure()
